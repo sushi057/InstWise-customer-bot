7617046ed7fa8bd65108dc2e8a506bcdbb0c2ba1
sushi057 <suvash2077@gmail.com>
2024-12-11 23:35:34 +0545

    feat: setup logging

--- config/logging.py
+++
@@ -1,9 +0,0 @@
-import json
-import pathlib
-import logging.config
-
-def setup_logging():
-    config_file = pathlib.Path("config/logging.json")
-    with open(config_file, "r") as f:
-        config = json.load(f)
-    logging.config.dictConfig(config)
