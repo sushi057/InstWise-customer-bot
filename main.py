be0074cfce9a7fac13a03b68acdad2162ea6099e
sushi057 <suvash2077@gmail.com>
2025-01-07 20:08:50 +0545

    feat: add /public/ask

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from typing import Optional
-from typing import Literal
+from typing import Optional, Literal
 
+import uvicorn
+from pydantic import EmailStr
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
+from langchain_core.runnables import RunnableConfig
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.errors import GraphRecursionError
 
@@ -18,7 +20,7 @@ app = FastAPI()
 app.include_router(customer.router, prefix="/customer", tags=["customer"])
 app.include_router(outreach.router, prefix="/outreach", tags=["outreach"])
 
-session_graph_cache = {"session_id": None, "graph": None, "customer_id": None}
+session_graph_cache = {"session_id": "", "graph": None, "customer_id": ""}
 
 session_object = {
     "session_id": None,
@@ -35,18 +37,17 @@ async def root():
 @app.get("/ask")
 async def ask_support(
     query: str,
-    user_email: str,
+    user_email: EmailStr,  # Optional
     org_id: str,
+    api_type=Literal["insights", "support"],
     session_id: Optional[str] = None,
-    customer_id: Optional[str] = None,
-    api_type: str = Literal["insights", "support"],
+    customer_id: Optional[str] = None,  # from query_database tool
 ):
-
     if not session_id:
-        session_id = get_session_id()
+        new_session_id = get_session_id()
 
     if session_graph_cache["session_id"] != session_id:
-        session_graph_cache["session_id"] = session_id
+        session_graph_cache["session_id"] = new_session_id
         new_memory = MemorySaver()
 
         # Check API type
@@ -56,7 +57,6 @@ async def ask_support(
             )
         elif api_type == "insights":
             session_graph_cache["graph"] = create_insights_graph(memory=new_memory)
-        # Add user_info state here
 
     graph = session_graph_cache["graph"]
 
@@ -65,7 +65,7 @@ async def ask_support(
             "thread_id": session_id,
             "user_email": user_email,
             "customer_id": customer_id,
-            "token": session_id + "_" + org_id,
+            "token": session_graph_cache["session_id"] + "_" + org_id,
         }
     }
 
@@ -89,4 +89,59 @@ async def ask_support(
         "message": PlainTextResponse(messages[-1]),
         "session_id": session_id,
         "customer_id": get_customer_id(),
+    }
+
+
+@app.get("/public/ask")
+async def ask_public_chat(
+    query: str,
+    org_id: str,
+    # customer_id: Optional[str],
+    session_id: Optional[str] = None,
+    # user_email: Optional[EmailStr],
+):
+    """
+    Chat API for public customers.
+    This API only has customer_support agent.
+    """
+
+    # Check if new session and create a new graph with new memory
+    if not session_id:
+        session_id = get_session_id()
+        session_graph_cache["session_id"] = session_id
+        new_memory = MemorySaver()
+
+        session_graph_cache["graph"] = create_graph(org_id=org_id, memory=new_memory)
+
+    graph = session_graph_cache["graph"]
+
+    config = RunnableConfig(
+        configurable={
+            "thread_id": session_id,
+        }
+    )
+
+    messages = []
+    try:
+        async for event in graph.astream(
+            {"messages": [("user", query)]},
+            config,
+            stream_mode="values",
+        ):
+            event["messages"][-1].pretty_print()
+            messages.append(event["messages"][-1].content)
+    except GraphRecursionError:
+        return {
+            "message": "Graph Recursion Error, Please try again.",
+            "session_id": session_id,
+            "customer_id": get_customer_id(),
+        }
+    except Exception as e:
+        return {"error": str(e), "session_id": session_id}
+
+    return {
+        "message": PlainTextResponse(messages[-1]),
+        "session_id": session_id,
+        "customer_id": get_customer_id(),
+        "user_email": None,
     }
