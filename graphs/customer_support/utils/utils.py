c4ba96ef7765cdae71f29d24fe885d4cf6adf5d1
sushi057 <suvash2077@gmail.com>
2025-01-12 20:35:15 +0545

    chore: cleanup

--- graphs/customer_support/utils/utils.py
+++ graphs/customer_support/utils/utils.py
@@ -1,6 +1,5 @@
 import os
-import json
-from typing import Callable
+import re
 import requests
 import secrets
 
@@ -9,71 +8,21 @@ from langchain_core.messages import ToolMessage
 from langgraph.prebuilt import ToolNode
 from langchain_core.runnables import RunnableLambda
 
-from graphs.customer_support.states.state import State
 
-planhat_mock_api_path = os.path.abspath("data/planhat_mock.json")
-# planhat_mock_api_path = "planhat_mock.json"
-# os.chdir(os.path.dirname(planhat_mock_api_path))
-
-
-def load_json():
-    with open(planhat_mock_api_path, "r") as f:
-        response = json.load(f)
-        return response
-
-
-def create_entry_node(assistant_name: str, new_dialog_state: str) -> Callable:
-    def entry_node(state: State) -> dict:
-        tool_call_id = state["messages"][-1].tool_calls[0]["id"]
-        return {
-            "messages": [
-                ToolMessage(
-                    content=f"The assistant is now the {assistant_name}. Reflect on the above conversation between the primary assistant and the user."
-                    f" The user's intent is unsatisfied. Use the provided tools to assist the user. Remember, you are {assistant_name},"
-                    " and the investigation, rag and other other action is not complete until after you have successfully invoked the appropriate tool."
-                    " If the user changes their mind or needs help for other tasks, call the CompleteOrEscalate function to let the primary primary assistant take control."
-                    " Do not mention who you are - just act as the proxy for the assistant.",
-                    tool_call_id=tool_call_id,
-                )
-            ],
-            "dialog_state": new_dialog_state,
-        }
-
-    return entry_node
-
-
-# This node will be shared for exiting all specialized assistants
-def pop_dialog_state(state: State) -> dict:
-    """Pop the dialog stack and return to the main assistant.
-
-    This lets the full graph explicitly track the dialog flow and delegate control
-    to specific sub-graphs.
+def get_valid_email(input_str: str) -> str:
     """
-    messages = []
-    if state["messages"][-1].tool_calls:
-        # Note: Doesn't currently handle the edge case where the llm performs parallel tool calls
-        messages.append(
-            ToolMessage(
-                content="Resuming dialog with the primary assistant. Please reflect on the past conversation and assist the user as needed.",
-                tool_call_id=state["messages"][-1].tool_calls[0]["id"],
-            )
-        )
-    return {
-        "dialog_state": "pop",
-        "messages": messages,
-    }
+    Extract and return a valid email from the input string.
+    """
+    email_regex = r"[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+"
+    match = re.search(email_regex, input_str)
+    if match:
+        return match.group(0)
+    return ""
 
 
 def handle_tool_error(state) -> dict:
     """
     Handle tool error and return a dictionary containing error messages.
-
-    Args:
-        state: A dictionary representing the state of the tool.
-
-    Returns:
-        A dictionary containing error messages.
-
     """
     error = state.get("error")
     tool_calls = state["messages"][-1].tool_calls
@@ -91,12 +40,6 @@ def handle_tool_error(state) -> dict:
 def create_tool_node_with_fallback(tools: list) -> dict:
     """
     Creates a tool node with fallbacks.
-
-    Args:
-        tools (list): A list of tools.
-
-    Returns:
-        dict: The tool node with fallbacks.
     """
     return ToolNode(tools).with_fallbacks(
         [RunnableLambda(handle_tool_error)], exception_key="error"
