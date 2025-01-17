7291423089bed417160e5fd16ef2b996f52cc6a4
sushi057 <suvash2077@gmail.com>
2025-01-17 22:45:40 +0545

    feat: add header for organization api

--- utils/utils.py
+++ utils/utils.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv
 # Load Backend APi
 load_dotenv()
 instwise_backend_url = os.environ["INSTWISE_BACKEND_URL"]
-setting_api_key = os.environ["SETTING_API_KEY"]
+instwise_secret_key = os.environ["INSTWISE_SECRET_KEY"]
+# setting_api_key = os.environ["SETTING_API_KEY"]
 
 
 def fetch_organization_details(org_id: str):
     """Fetch latest organization details from the backend API."""
-    url = f"{instwise_backend_url}/organizationDetail/{org_id}/"
-    headers = {"accept": "*/*", "x-api-key-local": setting_api_key}
+    url = f"{instwise_backend_url}/api/v1/organization?organization={org_id}"
+    headers = {"accept": "*/*", "x-api-secret-key": instwise_secret_key}
 
     try:
         response = requests.get(url, headers=headers)
