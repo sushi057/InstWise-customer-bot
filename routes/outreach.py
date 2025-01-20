cbc9878a1e2e1f3a18276c79a0107fe59eb3bd26
sushi057 <suvash2077@gmail.com>
2025-01-20 23:50:26 +0545

    chore: add type casting

--- routes/outreach.py
+++ routes/outreach.py
@@ -1,28 +1,28 @@
 import os
-import requests
-from typing import Union
+from typing import Union, cast
 
+import requests
 from fastapi import APIRouter, HTTPException
 from langchain_openai import ChatOpenAI
 
 from graphs.customer_insights.tools.tools import create_nl2sql_tool
 from models.outreach import (
-    PersonalizedEmailsList,
     CustomerListWithNegativeSentiments,
     GenerateEmailReplyRequest,
     GenerateEmailReplyResponse,
     GenerateEmailsRequest,
     GenerateEmailsResponse,
+    PersonalizedEmailsList,
 )
 from prompts.outreach import (
     customers_with_negative_sentiments_prompt,
     personalized_emails_prompt,
 )
-from utils.utils import fetch_organization_details
+from utils.helpers import fetch_organization_details
 
 router = APIRouter(tags=["outreach"])
 
-RAG_API_URL = os.getenv("RAG_API_URL")
+RAG_API_URL = os.getenv("RAG_API_URL", "")
 rag_api_headers = {"X-API-KEY": f"{os.getenv('X_API_KEY')}"}
 
 llm = ChatOpenAI(model="gpt-4o")
@@ -76,21 +76,29 @@ async def send_email(request: GenerateEmailsRequest):
         fetch_sentiment_query = "Fetch everything in tickets, tickets comments, conversation and feedbacks for 5 customers."
         sentiment_response = query_database(fetch_sentiment_query)
 
-        customers_with_negative_sentiments = (
+        customers_with_negative_sentiments_chain = (
             customers_with_negative_sentiments_prompt
             | structured_llm_for_negative_sentiments
-        ).invoke(
-            {
-                "customer_sentiments": sentiment_response,
-            }
+        )
+        customers_with_negative_sentiments = cast(
+            CustomerListWithNegativeSentiments,
+            customers_with_negative_sentiments_chain.invoke(
+                {
+                    "customer_sentiments": sentiment_response,
+                }
+            ),
         )
 
-        personalized_emails = (
+        personalized_emails_chain = (
             personalized_emails_prompt | structured_llm_for_personalized_emails
-        ).invoke(
-            {
-                "customers_with_negative_sentiments": customers_with_negative_sentiments.customers,
-            }
+        )
+        personalized_emails = cast(
+            PersonalizedEmailsList,
+            personalized_emails_chain.invoke(
+                {
+                    "customers_with_negative_sentiments": customers_with_negative_sentiments.customers
+                }
+            ),
         )
 
         if personalized_emails.emails:
